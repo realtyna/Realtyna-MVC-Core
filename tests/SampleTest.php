f35a257b4b2cf5b824d66fe1f1cab45f5eaf9427
Chandler Realtyna <chandler.p@realtyna.net>
2023-01-06 11:17:49 -0500

    add PHPUnit

---
+++ tests/SampleTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Sample_Plugin
+ */
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+    /**
+     * A single example test.
+     */
+    public function test_if_config_is_right() {
+
+    }
+}
